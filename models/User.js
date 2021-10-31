69d897a5917a022be17a0ecdb13885eb97748c19
rochiAmaya <rochiamaya@gmail.com>
2021-10-31 18:06:55 -0300

    nuevo modelo de usuario integrado a login spotify

--- models/User.js
+++ models/User.js
@@ -1,29 +1,32 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const UserSchema = new Schema({
-    firstName: {
+    name: {
         type: String,
         required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    userName: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true,
-        match: /(?=.*[a-zA-Z])(?=.*[0-9]+).*/,
-        minlength: 8
     },
     email: {
         type: String,
         required: true,
         match: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
     },
+    spotifyClientId: {
+        type: Number,
+        required: true
+    },
+    spotifyToken: {
+        type: String,
+        required: true
+    },
+    spotifyRefreshToken: {
+        type: String,
+        required: true
+    },
+    routines: [
+        {
+            type: mongoose.Schema.Types.ObjectId, ref: 'Routines'
+        }
+    ],
     created: {
         type: Date,
         default: new Date()
